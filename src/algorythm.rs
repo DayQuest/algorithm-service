88f0821b4f38431e3425cd27b0571c4e90ccfcc0
Tiiita <tiiitamail@gmail.com>
2024-12-23 02:40:27 +0100

    Rewrite algo, Complete endpoint (unfinished)

--- src/algorythm.rs
+++ src/algorythm.rs
@@ -1,9 +1,9 @@
 use rand::Rng;
-use serde::{Deserialize, Serialize};
+use sqlx::{query_scalar, Error, MySqlPool};
 
 const VIRAL_SCORE: f64 = 10_000.;
 
-pub fn _next_vid<'a>(user: User, videos: Vec<Video>) -> &'a str {
+pub fn _next_vid<'a>(_user: User, videos: Vec<Video>) -> &'a str {
     if videos.is_empty() {
         return "No videos present to choose from.";
     }
@@ -14,29 +14,33 @@ pub fn _next_vid<'a>(user: User, videos: Vec<Video>) -> &'a str {
 pub fn calc_score(video: &Video) -> f64 {
     let mut score = 1.;
 
-    score += (video.likes as f64 / 10.).powf(1.11);
+    score += (video.upvotes as f64 / 10.).powf(1.11);
     score += (video.views as f64 / 10.).powf(1.05);
 
-    //Engagement Rate
     if video.views != 0 {
-        score *= video.likes as f64 / video.views as f64;
+        score *= video.upvotes as f64 / video.views as f64;
     }
 
-    //TODO: Add video age
+    if video.views != 0 {
+        let total_votes = video.upvotes + video.downvotes;
+        if total_votes != 0 {
+            let upvote_ratio = video.upvotes as f64 / total_votes as f64;
+            score *= upvote_ratio.powf(1.2);
+            
+            let downvote_impact = video.downvotes as f64 / video.views as f64;
+            score *= (1.0 - downvote_impact).max(0.5);
+        }
+    }
+
+    // TODO: Add video age
 
     normalize_score(&mut score, &VIRAL_SCORE, 0.995);
 
-    match video.state {
-        State::Boosted => score *= 2.,
-        State::Private | State::Banned => score = 0.,
-        State::Normal => {}
-    }
-
-    match &video.risk_level {
+    /*match &video.risk_level {
         RiskLevel::Sus => score *= 0.5,
         RiskLevel::Sus2 => score *= 0.2,
         RiskLevel::Normal => {}
-    }
+    }*/
 
     score *= rand::thread_rng().gen_range(0.240..0.250);
     score /= 3.;
@@ -57,53 +61,59 @@ fn normalize_score(score: &mut f64, target: &f64, threshold: f64) {
 
 pub fn personalize_score(user: User, video: &Video) -> f64 {
     let mut score = video.score;
-    if let Some(following) = user.following {
-        if following.contains(&video.user) {
-            score *= 1.03;
-        }
+    if user.following.contains(&video.user_id) {
+        score *= 1.03;
     }
 
     //Do not wanna show exact same videos
-    if let Some(liked_vids) = user.liked_videos {
-        if liked_vids.contains(&video.uuid) {
-            score *= 0.96;
-        }
+    if user.liked_videos.contains(&video.uuid) {
+        score *= 0.96;
     }
 
     score
 }
 
-pub struct User<'a> {
-    username: &'a str,
-    liked_videos: Option<Vec<String>>,
-    following: Option<Vec<String>>,
-    watched: Option<Vec<String>>,
-    uuid: String,
+pub struct User {
+    pub username: String,
+    pub liked_videos: Vec<String>,
+    pub following: Vec<String>,
+    pub uuid: String,
 }
 
-#[derive(Deserialize, Serialize)]
+impl User {
+    pub async fn from_db(uuid: String, db_pool: MySqlPool) -> Result<User, Error> {
+        let username = query_scalar("SELECT username FROM users WHERE uuid = ?")
+            .bind(&uuid)
+            .fetch_one(&db_pool)
+            .await?;
+
+        let liked_videos: Vec<String> =
+            query_scalar("SELECT * FROM liked_videos WHERE user_id = ?")
+                .bind(&uuid)
+                .fetch_all(&db_pool)
+                .await?;
+
+        let following: Vec<String> =
+            query_scalar("SELECT * FROM user_followed_users WHERE user_uuid = ?")
+                .bind(&uuid)
+                .fetch_all(&db_pool)
+                .await?;
+
+        Ok(Self {
+            username,
+            liked_videos,
+            following,
+            uuid,
+        })
+    }
+}
+
+#[derive(sqlx::FromRow)]
 pub struct Video {
     pub uuid: String,
-    pub user: String,
-    pub likes: u32,
+    pub user_id: String,
+    pub upvotes: u32,
+    pub downvotes: u32,
     pub views: u32,
     pub score: f64,
-    pub risk_level: RiskLevel,
-    pub state: State,
-}
-
-#[derive(Deserialize, Serialize)]
-pub enum State {
-    Normal,
-    Boosted,
-    Banned,
-    Private,
-}
-
-//Change to RiskLevel in code and db
-#[derive(Deserialize, Serialize)]
-pub enum RiskLevel {
-    Normal,
-    Sus,
-    Sus2,
 }
--- src/endpoint.rs
+++ src/endpoint.rs
@@ -1,23 +1,40 @@
 use std::sync::Arc;
 
-use axum::{Extension, Json};
-use log::info;
+use axum::{http::StatusCode, Extension, Json};
+use log::{error, info};
 use serde::{Deserialize, Serialize};
-use sqlx::MySqlPool;
+use sqlx::{query_as, MySqlPool};
 
 use crate::algorythm::{self, Video};
 
 #[derive(Deserialize, Serialize)]
-pub struct VideoScoreResponse {
+pub struct ScoreVideoResponse {
     score: f64,
 }
 
-pub async fn video_score(
-    Json(payload): Json<Video>,
-    Extension(_db_pool): Extension<Arc<MySqlPool>>,
-) -> Json<VideoScoreResponse> {
-    let score = algorythm::calc_score(&payload);
+#[derive(Deserialize, Serialize)]
+pub struct ScoreVideoRequest {
+    uuid: String,
+}
 
-    info!("Calculated {}'s score: {}", payload.uuid, score);
-    Json(VideoScoreResponse { score })
+pub async fn video_score(
+    Json(payload): Json<ScoreVideoRequest>,
+    Extension(db_pool): Extension<Arc<MySqlPool>>,
+) -> Result<Json<ScoreVideoResponse>, StatusCode> {
+    match query_as::<_, Video>("SELECT * FROM video WHERE uuid = ?")
+        .bind(&payload.uuid)
+        .fetch_one(&*db_pool)
+        .await
+    {
+        Ok(video) => {
+            let score = algorythm::calc_score(&video);
+
+            info!("Calculated {}'s score: {}", payload.uuid, score);
+            Ok(Json(ScoreVideoResponse { score }))
+        }
+        Err(why) => {
+            error!("Error retrieving video data: {}", why);
+            Err(StatusCode::NOT_FOUND)
+        }
+    }
 }
